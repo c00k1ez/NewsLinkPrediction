f11aec54765b7f05c7a02fde0d0cedf81bc71923
c00k1ez <egorplotnikov@mail.ru>
2020-07-06 16:53:25 +0300

    fixes

--- train.py
+++ train.py
@@ -34,8 +34,13 @@ if __name__ == "__main__":
     # step 4 : init model
     model = LightningModel(config)
     # -----------------------------------------------------
-    # step 5 : init trainer
+    # step 5 : init logger(s)
+    logger = None
+    if 'logger' in config:
+        logger = pl.loggers.CometLogger(**config['logger'])
+    # -----------------------------------------------------
+    # step 6 : init logger(s)
     trainer = pl.Trainer(**config['trainer'])
     # -----------------------------------------------------
-    # step 6 : train model
+    # step 7 : train model
     trainer.fit(model, **loaders)
