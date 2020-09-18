8000aa2c0fa16ad654b641ec640cad3616f12682
c00k1ez <egorplotnikov@mail.ru>
2020-09-18 16:13:20 +0300

    fix

--- src/models.py
+++ src/models.py
@@ -41,6 +41,7 @@ class BaselineSiameseNetwork(torch.nn.Module):
 
     def forward(self, batch):
         broadcast = batch['broadcast']
+        print(broadcast.shape)
         broadcast_mask = batch['broadcast_mask']
         news = batch['news']
         news_mask = batch['news_mask']
